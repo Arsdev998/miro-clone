a3dd00be30193ba697d17ecca3ff7c8ce5d7ae20
Arsdev998 <arsdev998@gmail.com>
2024-03-05 06:27:08 +0700

    modal

--- app/(dasboard)/_components/sidebar/index.tsx
+++ app/(dasboard)/_components/sidebar/index.tsx
@@ -1,7 +1,9 @@
+import { NewButton } from "./bew-button";
+
 const Sidebar = () => {
   return (
     <div className="fixed flex flex-col left-0 z-[1] bg-blue-950 h-full w-[60px] p-3 gap-y-4 text-white">
-      side
+      <NewButton/>
     </div>
   );
 };
